9d54b39e15158f70d66cf3ff216fe12c083b4e33
TheZoq2 <frans.skarman@gmail.com>
2016-01-18 22:04:19 +0100

    Add unit tests

---
+++ testing/BoundedTest.cpp
@@ -0,0 +1,37 @@
+#include "catch.hpp"
+#include "../BoundedQueue.h"
+
+TEST_CASE("Bounded queue sizes")
+{
+    BoundedQueue<int> queue(5);
+
+    //Ensure that the new queue is empty
+    REQUIRE(queue.getSize() == 0);
+
+    queue.enqueue(1);
+    queue.enqueue(3);
+    queue.enqueue(-1);
+    queue.enqueue(-1);
+    queue.enqueue(-1);
+
+    SECTION( "Correct contents")
+    {
+        REQUIRE(queue.getSize() == 5);
+
+        REQUIRE(queue.bottom() == -1);
+
+        REQUIRE(queue.peek(0) == 1);
+        REQUIRE(queue.peek(1) == 3);
+        REQUIRE(queue.peek(2) == -1);
+        REQUIRE(queue.peek(2) == -1);
+        REQUIRE(queue.peek(2) == -1);
+
+        REQUIRE(queue.dequeue() == 1);
+        REQUIRE(queue.dequeue() == 3);
+        REQUIRE(queue.dequeue() == -1);
+        REQUIRE(queue.dequeue() == -1);
+        REQUIRE(queue.dequeue() == -1);
+
+        REQUIRE(queue.getSize() == 0);
+    }
+}
